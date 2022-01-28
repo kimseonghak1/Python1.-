868bec2250d8ab834f717a4c1deec269ec745e36
DESKTOP-4TVSQF2\KSH <jip9821@cu.ac.kr>
2022-01-28 15:04:17 +0900

    주석

--- boolean.py
+++ boolean.py
@@ -20,6 +20,7 @@ hobby = "공놀이"
 print(name, "는 ", str(age), "살이며, ", hobby, "을 아주 좋아해요")
 print(name+"는 어른일까요? " + str(is_adult))
 
+# 주석은 ctrl + /
 print("우리집 강아지의 이름은 연탄이에요")
 print("연탄이는 4살이며, 산책을 아주 좋아해요")
 print("연탄이는 어른일까요? True")
