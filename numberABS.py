5acf7b7141a21e56b75f6d07528b8944c0d299b8
DESKTOP-4TVSQF2\KSH <jip9821@cu.ac.kr>
2022-01-28 16:32:21 +0900

    숫자처리함수

---
+++ numberABS.py
@@ -0,0 +1,11 @@
+print(abs(-5)) # 절대값. 5
+print(pow(4,2)) # 4의 2제곱. 4^2 = 16
+print(max(5,12)) # 두 숫자 중 큰 수. 12
+print(min(5,12)) # 두 숫자 중 작은 수. 5
+print(round(3.14)) # 반올림. 3
+print(round(4.99)) # 반올림. 5
+
+from math import * # math함수에 대한 모든것(*) 
+print(floor(4.99)) # 내림. 4
+print(ceil(3.14)) # 올림. 4
+print(sqrt(16)) # 제곱근 . 4
